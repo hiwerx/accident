fead7608f6f8829184beafe2e046b169eb182cb7
hiwerx <hiwer@outlook.com>
2022-09-18 14:21:11 +0800

    离开页面请求参数存入cookie，返回页面重新加载

--- src/main/resources/static/assets/js/myUtil.js
+++ src/main/resources/static/assets/js/myUtil.js
@@ -56,5 +56,35 @@ function getLabel(){
     return ''
 }
 
+// 参考 https://cloud.tencent.com/developer/article/1776252
+//https://blog.csdn.net/beekim/article/details/120099638
+/*****设置cookie*****/
+function setCookieDay(name,value,iDay){
+    let oDate = new Date();
+    oDate.setDate(oDate.getDate()+iDay);
+    document.cookie = name+'='+value+';expires='+oDate.toUTCString();
+}
+/*****设置cookie*****/
+function setCookieMin(name,value,minute){
+    let oDate = new Date();
+    oDate.setMinutes(oDate.getMinutes()+minute)
+    document.cookie = name+'='+value+';expires='+oDate.toUTCString();
+}
+/*****获取cookie*****/
+function getCookie(name){
+    let arr = document.cookie.split("; ");
+    for(let i=0;i<arr.length;i++){
+        let arr2 = arr[i].split("=");
+        if(arr2[0]==name){
+            return arr2[1]
+        }
+    }
+    return ""
+}
+/*****移除cookie*****/
+function removeCookie(name){
+    setCookieDay(name,1,-1);
+};
+
 
 
